9c78734aa1af4cb4810b46d4c8644c2ceaf5c0ac
MitchelMA <32548@ma-web.nl>
2022-08-15 16:04:38 +0200

    significant kleinere opslag-grootte

--- saveio/saveio.c
+++ saveio/saveio.c
@@ -4,6 +4,11 @@
 #include <stdbool.h>
 #include <string.h>
 
+typedef unsigned char _cell_hex_;
+
+_cell_hex_ cto_ch_(const Cell *cell);
+void htoc(const _cell_hex_ _hex_, Cell *out);
+
 int bin_write(const Field *field, int savefield)
 {
     if (field == NULL)
@@ -50,16 +55,17 @@ int bin_write(const Field *field, int savefield)
         return 0;
     }
 
-    Cell cells[field->size * field->size];
+    _cell_hex_ hexs[field->size * field->size];
     for (size_t y = 0; y < field->size; y++)
     {
         for (size_t x = 0; x < field->size; x++)
         {
             size_t index = y * field->size + x;
-            cells[index] = field->cells[y][x];
+            hexs[index] = cto_ch_(&field->cells[y][x]);
         }
     }
-    size_t written = fwrite(cells, sizeof(Cell), sizeof(cells) / sizeof(cells[0]), fp);
+
+    size_t written = fwrite(hexs, sizeof(_cell_hex_), field->size * field->size, fp);
     if (written != field->size * field->size)
     {
         fclose(fp);
@@ -102,10 +108,12 @@ int bin_read(Field *field)
         return 0;
     }
 
-    Cell cells[field->size * field->size];
-    size_t read = fread(cells, sizeof(Cell), sizeof(cells) / sizeof(cells[0]), fp);
+    _cell_hex_ hexs[field->size * field->size];
+    size_t read = fread(hexs, sizeof(_cell_hex_), field->size * field->size, fp);
     if (read != field->size * field->size)
     {
+        printf("Er ging iets mis bij het uitlezen\n");
+        printf("kon maar %d van de %d elementen uitlezen\n", read, field->size * field->size);
         fclose(fp);
         return 0;
     }
@@ -129,7 +137,7 @@ int bin_read(Field *field)
         for (size_t x = 0; x < field->size; x++)
         {
             size_t index = y * field->size + x;
-            field->cells[y][x] = cells[index];
+            htoc(hexs[index], &field->cells[y][x]);
         }
     }
 
@@ -141,4 +149,45 @@ int clear_save(void)
 {
     fclose(fopen("save", "w"));
     return 1;
+}
+
+/*
+    `Cell` to `_cell_hex_`
+    A neighbour counts as 8.
+    After calculating the correct value corresponding to the neighbour count,
+    the status can just be added as a value.
+
+    This will result in in the size of a character,
+    that than can get pushed to the .bin file as a single byte.
+
+    Thus
+    a `Cell` with a status-value of 6 (is a bomb and is flagged),
+    and a neighbour-count of 2, will result in a decimal value of 22.
+    Or a hexadecimal value of 32.
+*/
+_cell_hex_ cto_ch_(const Cell *cell)
+{
+    const int nmult = 8;
+    _cell_hex_ hex;
+    hex = nmult * cell->bombneighbours;
+    hex += cell->status;
+    return hex;
+}
+
+/*
+    `_cell_hex_` to `Cell`
+    By casting the` _hex_` to an unsigned integer,
+    and then dividing it by the neighbour-value, will calculate the neighbour-count.
+    The status can get calculated the same way, except that the `rest` value is needed after dividing.
+
+    Thus a hexadecimal value of 32 (decimal value of 22) will result in a `Cell` with a status-value of 6 (bomb and flagged),
+    and a neighbour-count of 2.
+*/
+void htoc(const _cell_hex_ _hex_, Cell *out)
+{
+    const int nmult = 8;
+    int neighbours = (unsigned int)_hex_ / nmult;
+    int status = (unsigned int)_hex_ % nmult;
+    out->bombneighbours = neighbours;
+    out->status = status;
 }
